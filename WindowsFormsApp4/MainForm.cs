3bfa73231097aa814855b2c3708dd3efa5499fe6
Imy4260 <ljj571127@gmail.com>
2026-02-27 13:38:42 +0900

    새로운 라이브러리 추

--- WindowsFormsApp4/MainForm.cs
+++ WindowsFormsApp4/MainForm.cs
@@ -52,6 +52,21 @@ namespace WindowsFormsApp4
 
         private void imageOpenToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            CameraForm cameraForm = GetDockForm<CameraForm>();
+            if (cameraForm is null)
+                return;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Title = "이미지 파일 선택";
+                openFileDialog.Filter = "Image Files|*.bmp;*.jpg;*.jpeg;*.png;*.gif";
+                openFileDialog.Multiselect = false;
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string filePath = openFileDialog.FileName;
+                    cameraForm.LoadImage(filePath);
+                }
+            }
 
         }
 
